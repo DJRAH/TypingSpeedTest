423d4988beeb4ed42b33d1a7fc059043a4c3827b
djallal rahmoune <djallalrahmoune@gmail.com>
2024-10-31 14:18:11 +0100

    GUI updated OK

--- App.py
+++ App.py
@@ -6,40 +6,62 @@ class App:
     def __init__(self):
 
         self.root = Tk()
-        #define geometry
+
+         #define geometry
         w = 1000
         self.h = 700
         ws = self.root.winfo_screenwidth()  
         hs = self.root.winfo_screenheight()
         xx = (ws/2) - (w/2)
         yy = (hs/2) - (self.h/2)
-        self.root.geometry('%dx%d+%d+%d' % (w-380, self.h, xx+150, yy))
+        self.root.geometry('%dx%d+%d+%d' % (w-500, self.h-40, xx+140, yy))
         self.root.resizable(False,False)
+
+        self.frame = ttk.Frame(self.root)
+        self.frame.pack()
+
+        #instruction
+        self.frame_desc = ttk.LabelFrame(self.frame, text="Description ")
+        self.frame_desc.grid(row=0, column=0, padx=20, pady=40)
+
         
-        self.label = ttk.Label(text="").grid(row=1, column=0, columnspan=4)
-        self.label1 = ttk.Label(text="      Type words bellow during one minute.").grid(row=2, column=0, columnspan=4)
-        self.label2 = ttk.Label(text="      Space between each world.").grid(row=3, column=0, columnspan=4)
-        self.label3 = ttk.Label(text="      Receive your score : CPM and WPM").grid(row=4, column=0, columnspan=4)
-        self.label4 = ttk.Label(text="").grid(row=5, column=0, columnspan=4)
-        self.label4 = ttk.Label(text="").grid(row=6, column=0, columnspan=4)
-
-       
-        self.CPM = Label(text="     CPM : ").grid(row=7, column=0)
-        self.CPM_txt = Label(text="   ", width=10, bg="white").grid(row=7, column=1)
-        self.WPM = Label(text="     WPM : ").grid(row=7, column=2)
-        self.WPM_txt = Label(text="   ", width=10, bg="white").grid(row=7, column=3)
-
-        #self.label5 = ttk.Label(text="",width=75).grid(row=7, column=0, columnspan=4)
-        self.label6 = ttk.Label(text="",width=75).grid(row=8, column=0, columnspan=4)
-        self.label7 = ttk.Label(text="",width=75).grid(row=9, column=0, columnspan=4)
-
-        self.time = Label(text="      CountDown : ").grid(row=10, column=1)
-        self.time_txt = Label(text="   ", width=10, bg="white").grid(row=10, column=2)
-
-        self.label8 = ttk.Label(text="",width=75).grid(row=12, column=0, columnspan=4)
-
-        self.parag = Text(width=50, height=10, highlightbackground="white").grid(row=13, column=0, columnspan=4)
-        self.type_text = Text(width=30, height=1.5, highlightbackground="#091057").grid(row=14, column=0, columnspan=4)
+        self.label1 = ttk.Label(self.frame_desc, text="Type words bellow during one minute.").grid(row=0, column=0)
+        self.label2 = ttk.Label(self.frame_desc, text="Space between each world.").grid(row=1, column=0, )
+        self.label3 = ttk.Label(self.frame_desc, text="Receive your score : CPM and WPM").grid(row=2, column=0)
         
+        for widget in self.frame_desc.winfo_children():
+            widget.grid_configure(padx=10, pady=5)
+
+        # timer
+        self.frame_timer = ttk.LabelFrame(self.frame, text="")
+        self.frame_timer.grid(row=0, column=1, padx=20, pady=10)
+
+        self.timerRest = Button(self.frame_timer, text="Restart", border=None).grid(row=0, column=0)
+        self.timerLabel = Label(self.frame_timer, text="", width=10, height=3,bg="white").grid(row=1, column=0)
+
+        #typing
+        self.frame_typing = ttk.LabelFrame(self.frame, text="")
+        self.frame_typing.grid(row=1, column=0,columnspan=2, padx=20, pady=0)
+
+        self.parag = Text(self.frame_typing ,width=50, height=12, highlightbackground="white").grid(row=0, column=0)
+        self.type_text = Text(self.frame_typing, width=30, height=1.5, highlightbackground="#091057").grid(row=1, column=0) 
+
+
+        # result
+        self.frame_res = ttk.LabelFrame(self.frame, text="Result ")
+        self.frame_res.grid(row=2, column=0,columnspan=2, padx=20, pady=40)
+
+        self.CPM = Label(self.frame_res, text="CPM").grid(row=0, column=0)
+        self.CPM_txt = Label(self.frame_res, text="", width=10, bg="white", height=2).grid(row=1, column=0)
+        self.WPM = Label(self.frame_res, text="WPM").grid(row=0, column=1)
+        self.WPM_txt = Label(self.frame_res, text="", width=10, bg="white",height=2).grid(row=1, column=1)
+
+        for widget in self.frame_res.winfo_children():
+            widget.grid_configure(padx=60, pady=5)
 
         self.root.mainloop()
+       
+
+        
+
+       
